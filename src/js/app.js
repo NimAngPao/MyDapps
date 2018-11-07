2c6a38f13e742227ee5d140f34bf086033668a11
Totsapon Nonsreekot <totsapon_n@BKC.CO.TH>
2018-11-07 11:41:24 +0700

    Create displayAccountInfo

--- src/js/app.js
+++ src/js/app.js
@@ -4,25 +4,31 @@ App = {
 
      init: function() {
 
-      var articlesRow = $('#articlesRow');
-      var articleTemplate = $('#articleTemplate');
+      // var articlesRow = $('#articlesRow');
+      // var articleTemplate = $('#articleTemplate');
 
-      articleTemplate.find('.panel-title').text('article 1');
-      articleTemplate.find('.article-description').text('Description for article 1');
-      articleTemplate.find('.article-price').text('10.23');
-      articleTemplate.find('.article-seller').text('0x123456');
+      // articleTemplate.find('.panel-title').text('article 1');
+      // articleTemplate.find('.article-description').text('Description for article 1');
+      // articleTemplate.find('.article-price').text('10.23');
+      // articleTemplate.find('.article-seller').text('0x123456');
 
-      articlesRow.append(articleTemplate.html());
+      // articlesRow.append(articleTemplate.html());
 
       return App.initWeb3();
      },
 
      initWeb3: function() {
-          /*
-           * Replace me...
-           */
+           App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+           
+           web3 = new Web3(App.web3Provider);
+           
+           App.displayAccountInfo();
+           
+           return App.initContract();
+     },
 
-          return App.initContract();
+     displayAccountInfo: function(){
+
      },
 
      initContract: function() {
