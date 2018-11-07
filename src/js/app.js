4a96e3f89fd14d037af9dda39184ca3d326aca37
Totsapon Nonsreekot <totsapon_n@BKC.CO.TH>
2018-11-07 11:33:07 +0700

    Revise

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,17 @@ App = {
 
      init: function() {
 
-            var articleRow = $('#articlesRow');
-            var articleTemplate = $('#articleTemplate');
+      var articlesRow = $('#articlesRow');
+      var articleTemplate = $('#articleTemplate');
 
-            articleTemplate.find('.panel-title').text('Order No. 1');
-            articleTemplate.find('.article-description'.text('Description 1'));
-            articleTemplate.find('.article-price').text('10.24');
-            articleTemplate.find('.article-seller').text('Seller Name');
+      articleTemplate.find('.panel-title').text('article 1');
+      articleTemplate.find('.article-description').text('Description for article 1');
+      articleTemplate.find('.article-price').text('10.23');
+      articleTemplate.find('.article-seller').text('0x123456');
 
-            articlesRow.append(articleTemplate.html());
+      articlesRow.append(articleTemplate.html());
 
-          return App.initWeb3();
+      return App.initWeb3();
      },
 
      initWeb3: function() {
